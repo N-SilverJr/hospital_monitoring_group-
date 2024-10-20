999bb59197cb2506b80fa160af23cff4d6977f17
N-SilverJr <s.nshuti@alustudent.com>
2024-10-20 17:19:05 +0200

    backup_archives.sh and heart_rate_monitor.sh updated

--- backup_archives.sh
+++ backup_archives.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
+# Variables
 archived_directory="archived_logs_group21"
 host="cc91e1bce9ac.7b3ea1d9.alu-cod.online"
 username="cc91e1bce9ac"
-
+# Create archived_directory if it doesn't exist
 if [ ! -d "$archived_directory" ]; then
 	mkdir "$archived_directory"
 else
-        echo "Directory already exist"
+        # If archived_directory is present, then print this
+	echo "Directory already exist"
 fi
-
+# Looping through archived files and moving them to the archives directory
 for i in heart_rate_log.txt_*; do
 	if [ ! -f "$i" ]; then
 		echo "There is no file!"
@@ -16,5 +18,6 @@ for i in heart_rate_log.txt_*; do
 		mv heart_rate_log.txt_* "$archived_directory"
 	fi
 done	
+# Backup the archived files in the remote server
 scp -r $archived_directory "$username@$host":/home/ 
 echo "Backup was succesfull"
--- heart_rate_monitor.sh
+++ heart_rate_monitor.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 
+# Prompt the user to enter a device name
 read -p "Enter device name: " device_name
 
+# Check if the heart_rate_log.txt file exists
 if [ ! -f  "heart_rate_log.txt" ]; then
+        # If the file does not exist, create it
         touch  "heart_rate_log.txt"
 else
+        # If the file already exists, print this
         echo "The file already exists!"
 fi
 
+# Start an infinite loop
 while true; do
+        # Get the current timestamp in the format "YYYY-MM-DD HH:MM:SS"
         time_stamp=$( date "+%Y-%m-%d %H:%M:%S")
+        # Generate a simulated heart rate value between 60 and 100
         simulated_rate=$(( RANDOM % 40 + 60 ))
+        # Append the timestamp, device name, and simulated heart rate to the log file
         echo "$time_stamp $device_name $simulated_rate" >> heart_rate_log.txt
+        # Wait for 1 second before the next iteration
         sleep 1
-done &
-echo "The PID is: $!"
+done & # Run the loop in the background
 
+# Display the process ID
+echo "The PID is: $!"
